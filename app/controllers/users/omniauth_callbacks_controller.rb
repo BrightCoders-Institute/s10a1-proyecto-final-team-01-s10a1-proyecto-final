9e92fc7fb58238cb3d61286b7860c2117ba47b15
ErikGmz <erikgomez198755@outlook.com>
2024-03-06 19:44:45 -0600

    Sign Up styles completion and fixed confirmation mails sending when signing up with Google or Facebook.
    Co-authored-by: Fernando de Jesús Arellano Velasco <farellano0@users.noreply.github.com>
    Co-authored-by: Ramón Díaz <ramondiaz21@users.noreply.github.com>

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,13 +8,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.create_from_provider_data(request.env['omniauth.auth'])
 
     if @user.persisted?
-      @user.skip_confirmation!
-      @user.save
-
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
     else
-      session["devise.google_oauth2_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
       flash[:error] = 'There was a problem signing you in through Google. Please register or try signing in later.'
       redirect_to new_user_registration_url
     end
@@ -24,13 +20,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.create_from_provider_data(request.env["omniauth.auth"])
 
     if @user.persisted?
-      @user.skip_confirmation!
-      @user.save
-
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra)
       flash[:error] = 'There was a problem signing you in through Facebook. Please register or try signing in later.'
       redirect_to new_user_registration_url
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
 
   def self.create_from_provider_data(provider_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+      user.skip_confirmation!
       user.email = provider_data.info.email
       user.password = Devise.friendly_token[0, 20]
     end
