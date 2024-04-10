23e17c59c2f7af56961ada08b322d2c01c367f88
ErikGmz <erikgomez198755@outlook.com>
2024-04-09 21:03:15 -0600

    Fixes in seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,16 +24,16 @@ details_list = %i[wifi kitchen working_place washing_mashine administration
                   dryer_machine hair_dryer television air_conditioning pet_friendly
                   garden_view]
 
+roles_list.each { |role| Role.find_or_create_by(name: role) }
+categories_list.each { |category| Category.find_or_create_by(name: category) }
+details_list.each { |detail| Detail.find_or_create_by(name: detail) }
+
 users_list.each do |user|
   db_user = User.find_or_create_by(email: user[:email])
   db_user.update(role_id: user[:role_id], password: '123123', password_confirmation: '123123')
   db_user.skip_confirmation!
   db_user.save!
 end
-
-roles_list.each { |role| Role.find_or_create_by(name: role) }
-categories_list.each { |category| Category.find_or_create_by(name: category) }
-details_list.each { |detail| Detail.find_or_create_by(name: detail) }
 
 30.times do |number|
   count = number + 1
@@ -53,8 +53,8 @@ details_list.each { |detail| Detail.find_or_create_by(name: detail) }
   reservation = Reservation.where(user_id: 32, accommodation_id: accommodation.id).first_or_initialize
   reservation.update(active: 1, dates_range: "2024/03/10 - 2024/03/12")
 
-  review = Review.where(title: "Review no. #{accommodation.id}").first_or_initialize
-  review.update(user_id: 32, accommodation_id: accommodation.id, rating: rand(0..5))
+  review = Review.where(user_id: 32, accommodation_id: accommodation.id, title: "Review no. #{accommodation.id}").first_or_initialize
+  review.update(rating: rand(0..5))
 
   description = ActionText::RichText.where(record_type: 'Review', record_id: review.id,
                                           name: 'content').first_or_initialize
